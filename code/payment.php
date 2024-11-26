d983f486751ce196ebfc1be117c025bc595575f8
Arun <arunarora@zapbuild.com>
2024-11-26 20:40:11 +0530

    commit dummy twilio creds

--- code/payment.php
+++ code/payment.php
@@ -4,6 +4,8 @@ $server = 'localhost';
 $username = 'root';
 $password = '';
 $dbname  = 'RMS';
+$twilio_key = "SK5f7ff7acb9esssfdaa5902c25970csss";
+$twilio_secret = "U4aBYrrryyejycAM8ms2595QSiVUm5Cg2";
 
 $conn = new mysqli($server, $username, $password, $dbname);
 if ($conn->connect_error) {
